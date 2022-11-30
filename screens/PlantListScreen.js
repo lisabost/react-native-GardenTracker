84ea747c54993770d53c2de3c0ff0c152f216449
Lisa Bost <bost.lisam@gmail.com>
2022-11-29 19:41:43 -0600

    create a plant list screen for our plant list

---
+++ screens/PlantListScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PlantListScreen = () => {
+  return (
+	<View>
+	  <Text>PlantListScreen</Text>
+	</View>
+  )
+}
+
+export default PlantListScreen
+
+const styles = StyleSheet.create({})
