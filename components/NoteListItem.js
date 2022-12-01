90955880acece1c7cc199c37cb77ae756ae26576
Lisa Bost <bost.lisam@gmail.com>
2022-12-01 11:43:54 -0600

    note list item - buttons for view, edit, delete - displays index and title

---
+++ components/NoteListItem.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { MaterialIcons } from "@expo/vector-icons";
+import React from 'react';
+
+const NoteListItem = (props) => {
+	const { index, note, deleteNote } = props;
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.indexContainer}>
+				<Text style={styles.index}>{index}</Text>
+			</View>
+			<View style={styles.itemContainer}>
+				<Text style={styles.item}>{note.title}</Text>
+				<TouchableOpacity onPress={() => {}}>
+					<MaterialIcons style={styles.icon} name="preview" size={18} color="000" />
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => {}}>
+					<MaterialIcons style={styles.icon} name="edit" size={18} color="000" />
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => deleteNote(note.id)}>
+					<MaterialIcons style={styles.icon} name="delete" size={18} color="000" />
+				</TouchableOpacity>
+			</View>
+		</View>
+	)
+}
+
+export default NoteListItem
+
+const styles = StyleSheet.create({
+	container: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+    indexContainer: {
+        backgroundColor: '#90C6EE',
+        borderRadius: 12,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+    },
+    index: {
+        color: '#000',
+        fontSize: 20,
+    },
+    itemContainer: {
+        backgroundColor: '#90C6EE',
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        minHeight: 50,
+    },
+    item: {
+        color: '#000',
+        width: '80%',
+        fontSize: 16,
+    },
+    icon: {
+        // marginLeft: 10,
+    },
+})
