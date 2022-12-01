b56f33383051e905df609bbc96b996182755ede1
Lisa Bost <bost.lisam@gmail.com>
2022-12-01 11:14:26 -0600

    Added note input screen to navigation

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -8,6 +8,7 @@ import HomeScreen from '../screens/HomeScreen';
 import PlantListScreen from '../screens/PlantListScreen';
 import { auth } from '../firebase';
 import GardenNotesScreen from '../screens/GardenNotesScreen';
+import NoteInputScreen from '../screens/NoteInputScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -35,6 +36,7 @@ const DrawerNavigator = () => {
 		{ !isLoggedIn && <Drawer.Screen options={{ headerTitle: '', headerStyle: { backgroundColor: '#90C6EE' } }} name="Start" component={MainStackNavigator} />}
 		{ isLoggedIn && <Drawer.Screen options={{ headerTitle: '', headerStyle: { backgroundColor: '#90C6EE' } }} name="Plant List" component={PlantListStackNavigator} />}
 		{ isLoggedIn && <Drawer.Screen options={{ headerTitle: '', headerStyle: { backgroundColor: '#90C6EE' } }} name="Garden Notes" component={GardenNotesScreen} /> }
+		{ isLoggedIn && <Drawer.Screen options={{headerTitle: '', headerStyle: { backgroundColor: '#90C6EE' } }} name="Add Garden Note" component={NoteInputScreen} /> } 
 		{ isLoggedIn && <Drawer.Screen options={{ headerTitle: '', headerStyle: { backgroundColor: '#90C6EE' } }} name="Sign Out" component={HomeScreen} /> }
 	</Drawer.Navigator>
   )
